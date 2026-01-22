5575d8b91f968ee9d33286a0ecdd76ef84f04004
Qiyu Li <qylee2006@gmail.com>
2026-01-22 21:55:53 +0800

    Complete Level-1

--- src/main/java/BlondeBlazer.java
+++ src/main/java/BlondeBlazer.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class BlondeBlazer {
     public static void main(String[] args) {
-        String logo = "---------------------------------------------\n";
-        System.out.println(logo + "Hello! I'm BlondeBlazer!");
+        String logo = "---------------------------------------------";
+        System.out.println(logo + "\n" + "Hello! I'm BlondeBlazer!");
         System.out.println("What can I do for you?\n");
-
-        System.out.println(logo + "Bye. Hope to see you again soon!\n");
         System.out.println(logo);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+
+            if (line.equals("bye")) {
+                System.out.println(logo + "\n" + "Bye. Hope to see you again soon!\n");
+                System.out.println(logo);
+                break;
+            }
+
+            System.out.println(logo);
+            System.out.println(line + "\n");
+            System.out.println(logo + "\n");
+        }
     }
 }
