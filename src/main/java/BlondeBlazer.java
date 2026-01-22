775797440b7daa907e4b27edf6e49e17bdb5f64c
Qiyu Li <e1484094@u.nus.edu>
2026-01-22 21:15:40 +0800

    Complete Level-0

---
+++ src/main/java/BlondeBlazer.java
@@ -0,0 +1,10 @@
+public class BlondeBlazer {
+    public static void main(String[] args) {
+        String logo = "---------------------------------------------\n";
+        System.out.println(logo + "Hello! I'm BlondeBlazer!");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println(logo + "Bye. Hope to see you again soon!\n");
+        System.out.println(logo);
+    }
+}
