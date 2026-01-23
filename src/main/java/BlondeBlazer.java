e566838a1400ff09a775c639884c2da1cc2b14f8
Qiyu Li <qylee2006@gmail.com>
2026-01-23 16:00:01 +0800

    Complete Level-2

--- src/main/java/BlondeBlazer.java
+++ src/main/java/BlondeBlazer.java
@@ -4,22 +4,42 @@ public class BlondeBlazer {
     public static void main(String[] args) {
         String logo = "---------------------------------------------";
         System.out.println(logo + "\n" + "Hello! I'm BlondeBlazer!");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
         System.out.println(logo);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
 
             if (line.equals("bye")) {
-                System.out.println(logo + "\n" + "Bye. Hope to see you again soon!\n");
+                System.out.println(logo);
+                System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(logo);
                 break;
             }
 
+            if (line.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("Well, I got nothing to list out. Try add something.");
+                }
+
+                else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            }
+
+            else {
+                tasks[taskCount] = line;
+                taskCount++;
+                System.out.println("added: " + line);
+            }
+
             System.out.println(logo);
-            System.out.println(line + "\n");
-            System.out.println(logo + "\n");
         }
     }
 }
